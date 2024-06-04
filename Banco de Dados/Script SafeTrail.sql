cf9058d0136fe5d1bf68d0c624557297d441399d
DiegoDueranGarcia <diego.garcia@sptech.school>
2024-06-04 14:28:31 -0300

    Site inserido na VM

--- Banco de Dados/Script SafeTrail.sql
+++ Banco de Dados/Script SafeTrail.sql
@@ -10,21 +10,6 @@ email varchar(50) not null,
 senha varchar(30) not null
 );
 
-CREATE TABLE Acompanhantes(
-idAcompanhante int primary key auto_increment,
-nome varchar(30)not null,
-cpf char(12) not null ,
-idade varchar(2) not null,
-fkMaiorIdade int,
-constraint FKACOMPANHANTE foreign key (fkMaiorIdade) references Acompanhantes(idAcompanhante)
-);
-
-CREATE TABLE Acompanhantes(
-idUsuario_Acompanhante int primary key auto_increment,
-fkUsuario int,
-fkAcompanhante int,
-constraint ChaveComposta primary key (idUsuario_Acompanhante,fkUsuario,fkAcompanhante)
-);
 
 
 
